52ab9a42fb910231c329f80ce4f180ee09d2eb89
Vadon <vadondaniel02@gmail.com>
2023-11-29 18:51:04 +0100

    continents enum for model

---
+++ src/app/continent.ts
@@ -0,0 +1,11 @@
+// continent.ts
+
+export enum Continent {
+    Africa = "Africa",
+    Antarctica = "Antarctica",
+    Asia = "Asia",
+    Europe = "Europe",
+    NorthAmerica = "North America",
+    Australia = "Australia",
+    SouthAmerica = "South America"
+}
