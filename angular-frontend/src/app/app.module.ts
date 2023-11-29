22050dc025cbfd7c6f4d17d4db525cdb1dce8471
Vadon <vadondaniel02@gmail.com>
2023-11-30 00:15:44 +0100

    dropdown menu for List and Add

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 import { CountryListComponent } from './country-list/country-list.component';
 import { CityListComponent } from './city-list/city-list.component';
 import { CountryDetailsComponent } from './country-details/country-details.component';
 import { CityDetailsComponent } from './city-details/city-details.component';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AddCountryComponent } from './add-country/add-country.component';
 import { AddCityComponent } from './add-city/add-city.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -31,7 +34,10 @@ import { AddCityComponent } from './add-city/add-city.component';
         FormsModule,
         AppRoutingModule,
         HttpClientModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatOptionModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
