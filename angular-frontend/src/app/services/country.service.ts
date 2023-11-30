97515d96834f5f4e7b2cbc727a83aabc557c3e12
Vadon <vadondaniel02@gmail.com>
2023-11-30 16:11:07 +0100

    Refactor country service to include continent mapping

--- angular-frontend/src/app/services/country.service.ts
+++ angular-frontend/src/app/services/country.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Country } from '../models';
+import { map } from 'rxjs/operators';
+import { Continent } from '../continent';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +15,23 @@ export class CountryService {
 
   // Fetch all countries
   getCountries(): Observable<Country[]> {
-    return this.http.get<Country[]>(this.baseUrl);
+    return this.http.get<Country[]>(this.baseUrl).pipe(
+      map(countries => countries.map(country => ({
+        ...country,
+        continent: Continent[country.continent.replace(" ", "") as keyof typeof Continent]
+      })))
+    );
   }
 
   // Fetch a country by ID
   getCountryById(id: number): Observable<Country> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Country>(url);
+    return this.http.get<Country>(url).pipe(
+      map(country => ({
+        ...country,
+        continent: Continent[country.continent.replace(" ", "") as keyof typeof Continent]
+      }))
+    );
   }
 
   // Add a new country
