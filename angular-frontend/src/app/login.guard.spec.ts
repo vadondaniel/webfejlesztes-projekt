b1463af1afabdb8ecc6485234906dfc69cf0a66d
Vadon <vadondaniel02@gmail.com>
2023-12-04 15:02:33 +0100

    Fix import statement in loginGuard spec

--- angular-frontend/src/app/login.guard.spec.ts
+++ angular-frontend/src/app/login.guard.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { loginGuard } from './login.guard';
+import { LoginGuard } from './login.guard';
 
 describe('loginGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => loginGuard(...guardParameters));
+      TestBed.runInInjectionContext(() => LoginGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
